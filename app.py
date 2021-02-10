1208375b0caebe20ba3b99cb03e888cd3a133fd2
saraselis <saras1@hotmail.com.br>
2021-02-09 21:58:44 -0300

    ADD: Streamlit

---
+++ app.py
@@ -0,0 +1,78 @@
+import os
+import pandas as pd 
+import streamlit as st
+import time
+import torch
+
+from simpletransformers.classification import MultiLabelClassificationModel
+
+from tratador_texto import TrataTexto
+
+PATH_PESO = "./Pesos/bert_multicass"
+
+# argumentos do modelo
+args = {
+    'reprocess_input_data': True,
+    'overwrite_output_dir': True,
+    'evaluate_during_training': True,
+    'use_multiprocessing': False,
+    "num_train_epochs": 1,
+    "train_batch_size": 16,
+    "eval_batch_size": 16,
+}
+
+# verificando disponibilidade do cuda
+cuda_available = torch.cuda.is_available()
+print(cuda_available)
+
+# importando o modelo
+model_vencedor = MultiLabelClassificationModel('bert',
+                                               PATH_PESO,
+                                               args=args,
+                                               use_cuda=True)
+
+tratador = TrataTexto()
+
+st.title("Classificação Multi-label com BERT 💻")
+
+st.text("O dataset utilizado é um dataset de perguntas do Stack Overflow.")
+st.markdown("[StackOverflow](https://stackoverflow.com/)")
+
+st.subheader('Insira aqui sua pergunta à ser classificada 🙋:')
+
+pergunta_padrao = "Vi esse vídeo (link abaixo) e gostaria de construir um elemento que somente aparecesse quando a rolagem atingir o 'ponto que toque nele'. Creio que deve ser javascript. Se alguém souber ficaria muito grato :D Tentei linkar os arquivos e add a div no meu projeto mas ela simplesmente sumiu.  OBS: Ele mostra o efeito em 3:46  https://www.youtube.com/watch?v=tdgDr_icGdo"
+
+message = st.text_area('Temos uma pergunta para testes:', pergunta_padrao)
+
+html_page = """
+<div style="background-color:tomato; padding:5px">
+    <p style="font-size:30px; text-align:center">Resultados</p>
+</div>
+
+"""
+
+if st.button("Enviar"):
+    st.success("A pergunta recebida será classificada!")
+    time.sleep(1)
+
+    st.markdown(html_page, unsafe_allow_html=True)
+
+    try:
+        texto_tratado = tratador.fix_text(message)
+        predictions, probs = model_vencedor.predict([texto_tratado])
+
+        df_data = [
+            [predictions[0][0], probs[0][0], 'NodeJs'],
+            [predictions[0][1], probs[0][1], 'Jquery'],
+            [predictions[0][2], probs[0][2], 'Html'],
+            [predictions[0][3], probs[0][3], 'Angular']
+        ]
+
+        st.text('')
+        df = pd.DataFrame(df_data)
+        df.columns = ["Predição", "Probabilidade", "Label"]
+        st.dataframe(df)
+
+    except Exception as error:
+        st.text('')
+        st.exception(error)
---
+++ tratador_texto.py
@@ -0,0 +1,192 @@
+import logging
+import nltk
+import re
+import sys
+
+from pyfields import field
+
+remove_accent_ = {'a': r'[\xE0-\xE6]', 'e': r'[\xE8-\xEB]',
+                  'i': r'[\xEC-\xEF]',
+                  'o': r'[\xF2-\xF6]',
+                  'u': r'[\xF9-\xFC]', 'c': r'[\xE7]', 'y': r'[\xFD|\xFF]'}
+
+remove_stop_ = set(nltk.corpus.stopwords.words('portuguese'))
+
+string_pont_ = '[!"#$%&\'()*+-/:;<=>?@[\\]^_`{|}~—"\'“”ª«»£@#$%¢¬&*§º]'
+
+logger = logging.getLogger("BERT Multi-Label")
+logger.setLevel(logging.DEBUG)
+handler = logging.StreamHandler(sys.stderr)
+formatter = logging.Formatter('%(name)s - %(levelname)s - [+] ------- %(message)s -------') 
+handler.setFormatter(formatter)
+logger.handlers = [handler]
+
+
+class TrataTexto:
+    '''
+        Classe destinada ao tratamento do texto enviado para
+            realização da predição.
+
+        Atributos da classe:
+            \t* remove_accent_str - Regex para remoção de acentos
+            \t* string_pont - Regex para remoção de pontuação
+            \t* remove_stop -  Stopwords NLTK
+    '''
+
+    remove_accent_str: str = field(doc="String regex para remoção de acentos.",
+                                  default=remove_accent_, check_type=True,
+                                  type_hint=dict)
+    string_pont: str = field(doc="String regex para remoção de pontos.",
+                             default=string_pont_, check_type=True,
+                             type_hint=str)
+    remove_stop: list = field(doc="Nltk Stopwords.",
+                              default=remove_stop_, type_hint=dict)
+    count: int = field(doc="Contador para textos", default=1, type_hint=int)
+
+    def remove_accent(self, text: str) -> str:
+        """Remove os acentos das palavras do texto.
+
+        :param text: texto
+        :type text: str
+        :return: texto sem acentos
+        :rtype: str
+        """
+
+        for letter, regex in self.remove_accent_str.items():
+            text = re.sub(regex, letter, text)
+
+        return text
+
+    def remove_pont(self, texto: str) -> str:
+        """Remove pontuação.
+
+        :param texto: texto
+        :type texto: str
+        :return: texto sem pontuação
+        :rtype: str
+        """
+
+        text = re.sub(self.string_pont, ' ', texto)
+
+        return text
+
+    def remove_stopwords(self, text: str, custom_stopwords: str = None) -> str:
+        """Remove as stopwords do texto.
+
+        :param text: texto
+        :type text: str
+        :param custom_stopwords: stopwords customizadas, defaults to None
+        :type custom_stopwords: str, optional
+        :return: str
+        :rtype: texto sem stopwords
+        """
+
+        if custom_stopwords:
+            stopwords = custom_stopwords
+        else:
+            stopwords = self.remove_stop
+
+        text_tokenized = nltk.tokenize.word_tokenize(text)
+
+        for index, word in enumerate(text_tokenized):
+            if word in stopwords:
+                text_tokenized[index] = ' '
+        text = ' '.join(text_tokenized)
+
+        return text
+
+    @staticmethod
+    def lower_case(text: str) -> str:
+        """Deixa todas as letras em minúsculo.
+
+        :param text: texto
+        :type text: str
+        :return: texto em minúsculas
+        :rtype: str
+        """
+
+        text = text.lower()
+
+        return text
+
+    @staticmethod
+    def remove_lines(text: str) -> str:
+        """Remove linhas duplicadas.
+
+        :param text: texto
+        :type text: str
+        :return: texto sem linhas duplicadas
+        :rtype: str
+        """
+
+        text = re.sub(r'\\n', ' ', text)
+        text = re.sub(r'\n', ' ', text)
+
+        return text
+
+    @staticmethod
+    def remove_spaces(text: str) -> str:
+        """Remove espaços duplicados.
+
+        :param text: texto
+        :type text: str
+        :return: texto sem espaços duplicados.
+        :rtype: str
+        """
+
+        text = ' '.join(text.split())
+        return text
+
+    @staticmethod
+    def remove_emoji(text):
+        """Remove os emojis no texto.
+
+        :param text: texto
+        :type text: str
+        :return: texto sem espaços duplicados.
+        :rtype: str
+        """
+
+        regrex_pattern = re.compile(pattern = "["
+        u"\U0001F600-\U0001F64F"
+        u"\U0001F300-\U0001F5FF"
+        u"\U0001F680-\U0001F6FF"
+        u"\U0001F1E0-\U0001F1FF"
+                           "]+", flags = re.UNICODE)
+        return regrex_pattern.sub(r'',text)
+
+    @staticmethod
+    def remove_links(text):
+        """Remove os links do texto.
+
+        :param text: texto
+        :type text: str
+        :return: texto sem espaços duplicados.
+        :rtype: str
+        """
+        text = re.sub(r"http\S+", "", text)
+
+        return text
+
+    def fix_text(self, text):
+        """Chama os outros métodos na ordem para o tratamento do texto.
+
+        :param text: texto
+        :type text: str
+        :return: texto sem espaços duplicados.
+        :rtype: str
+        """
+
+        logger.info(f"Iniciando tratamento do texto número: {self.count}")
+        tratador = TrataTexto()
+        text = tratador.remove_links(text)
+        text = tratador.remove_pont(text)
+        text = tratador.remove_accent(text)
+        text = tratador.lower_case(text)
+        text = tratador.remove_lines(text)
+        text = tratador.remove_stopwords(text)
+        text = tratador.remove_spaces(text)
+        text = tratador.remove_emoji(text)
+
+        self.count += 1
+        return text
